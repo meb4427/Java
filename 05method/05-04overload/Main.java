83567e69208f1b83aca04b239435621f05b6ab55
Sadanori Maeba <sadanori.maeba@email.com>
2024-05-09 20:17:05 +0900

    05-04overloadまで終了

---
+++ 05method/05-04overload/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static int add(int x, int y, int z) {
+        return x + y + z;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(10, 20, 40));
+
+    }
+}
