ecb2a8de7704fe7477bb68919f35927b435ee8f5
Sadanori Maeba <sadanori.maeba@email.com>
2024-05-09 21:56:09 +0900

    05-08task02まで終了

---
+++ 05method/05-08task02/Main.java
@@ -0,0 +1,16 @@
+// package 05-08task02;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+            email("依頼", "aaa@example.com", "ご確認をお願いいたします")
+        );
+    }
+
+    public static String email(String title, String address, String text) {
+        String str = address + "に，以下のメールを送信しました。\n"
+                                + "件名:" + title + "\n"
+                                + "本文:" + text;
+        return str;
+    }
+}
---
+++ 05method/05-08task02/Main3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        email("確認依頼", "aaa@example.com", "ご確認のほど，よろしくお願いいたします");
+    }
+
+    public static void email(String title, String address, String text) {
+        System.out.println(address + "に，以下のメールを送信しました。");
+        System.out.println("件名:" + title);
+        System.out.println("本文:" + text);
+    }
+}
