0850375e257677c7b6a2b7ec7313b01332eaccc8
Sadanori Maeba <sadanori.maeba@gmail.com>
2024-06-06 16:58:19 +0900

    chapter11の３週目の終了

---
+++ 01basics/11advancedInheritance/03/test11-1/Computer.java
@@ -0,0 +1,12 @@
+public class Computer extends TangibleAsset {
+    String makerName;
+
+    public Computer(String name, int price, String color, String makerName, double weight) {
+        super(name, price, color, weight);
+        this.makerName = makerName;
+    }
+
+    public String getMakerName() {
+        return this.makerName;
+    }
+}
